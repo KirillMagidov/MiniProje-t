359493571b67cc7c94bfe01cbaaeee9deb197a59
Kirill <kirill.magidov@edu.fhdw.de>
2026-05-20 18:21:37 +0200

    FlottenmanagerService

--- src/main/java/de/easyscoot/service/EScooterService.java
+++ src/main/java/de/easyscoot/service/EScooterService.java
@@ -1,4 +1,26 @@
 package de.easyscoot.service;
 
-public class EScooterService {
+import de.easyscoot.model.EScooter;
+import de.easyscoot.repository.IScooterRepository;
+
+import java.util.List;
+
+
+public class EScooterService implements IFlottenmanagerService {
+
+    private IScooterRepository scooterRepository;
+
+    public EScooterService(IScooterRepository scooterRepository) {
+        this.scooterRepository = scooterRepository;
+    }
+
+    @Override
+    public List<EScooter> getAllScooters() {
+        return scooterRepository.findAll();
+    }
+
+    @Override
+    public EScooter getScooterDetails(String id) {
+        return scooterRepository.findById(id);
+    }
 }
