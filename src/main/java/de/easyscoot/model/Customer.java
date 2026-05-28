27a0eb9d0ac8acab4bbc1dca6b3b3c3896b3f413
philipp2705 <philippkremeike@gmail.com>
2026-05-28 11:54:44 +0200

    User extends Costumer/Flottenmanager/ServiceEmployee

--- src/main/java/de/easyscoot/model/Customer.java
+++ src/main/java/de/easyscoot/model/Customer.java
@@ -1,44 +1,28 @@
 package de.easyscoot.model;
 
-public class Customer {
+public class Customer extends User {
 
-    private String foreName;
-    private String name;
     private String street;
     private Integer streetNumber;
     private String location;
     private Integer plz;
-    private String email;
-    private String password;
 
-    public Customer () {
+    public Customer() {
 
     }
 
 
-
-    public Customer(String foreName, String name, String street,
+    public Customer(String forename, String name, String street,
                     Integer streetNumber, String location,
                     Integer plz, String email, String password) {
 
-        this.foreName = foreName;
-        this.name = name;
+        super(forename, name, email, password);
         this.street = street;
         this.streetNumber = streetNumber;
         this.location = location;
         this.plz = plz;
-        this.email = email;
-        this. password = password;
     }
 
-
-    public String getForeName() {
-        return foreName;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     public String getStreet() {
         return street;
@@ -56,22 +40,6 @@ public class Customer {
         return plz;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword (){
-        return password;
-    }
-
-
-    public void setForeName(String foreName) {
-        this.foreName = foreName;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public void setStreet(String street) {
         this.street = street;
@@ -87,13 +55,5 @@ public class Customer {
 
     public void setPlz(Integer plz) {
         this.plz = plz;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword (String password) {
-        this.password = password;
     }
 }
--- src/main/java/de/easyscoot/model/ServiceEmployee.java
+++ src/main/java/de/easyscoot/model/ServiceEmployee.java
@@ -1,52 +1,11 @@
 package de.easyscoot.model;
 
-public class ServiceEmployee {
-    private String foreName;
-    private String name;
-    private String email;
-    private String password;
-
+public class ServiceEmployee extends User {
     public ServiceEmployee() {
 
     }
 
-    public ServiceEmployee(String forname, String name, String email, String password) {
-        this.foreName = forname;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getForeName() {
-        return foreName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-    public void setForename(String forename) {
-        this.foreName = forename;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public ServiceEmployee(String forename, String name, String email, String password) {
+        super(forename, name, email, password);
     }
 }
