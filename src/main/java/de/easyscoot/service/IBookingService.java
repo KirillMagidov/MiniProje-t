217bc054eea82452822d2af032419c415e8d9ff8
philipp2705 <philippkremeike@gmail.com>
2026-06-04 15:10:54 +0200

    Interfaces für BookingService und ValidationService

--- src/main/java/de/easyscoot/service/IBookingService.java
+++ src/main/java/de/easyscoot/service/IBookingService.java
@@ -5,9 +5,9 @@ import de.easyscoot.model.Booking;
 import java.util.List;
 
 public interface IBookingService {
-    public Booking startRide(String customerId, String scooterID);
+    Booking startRide(String customerId, String scooterID);
 
-    public Booking endRide(String bookingID);
+    Booking endRide(String bookingID);
 
-    public List<Booking> getCustomerHistory(String customerID);
+    List<Booking> getCustomerHistory(String customerID);
 }
--- src/main/java/de/easyscoot/service/IValidationService.java
+++ src/main/java/de/easyscoot/service/IValidationService.java
@@ -1,9 +1,9 @@
 package de.easyscoot.service;
 
 public interface IValidationService {
-    public boolean isValid(String email);
+    boolean isValid(String email);
 
-    public boolean isValidAdresse(String street, Integer streetNumber, Integer plz, String location);
+    boolean isValidAdresse(String street, Integer streetNumber, Integer plz, String location);
 
-    public boolean isValidPassword(String password);
+    boolean isValidPassword(String password);
 }
